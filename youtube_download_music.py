c743fc0eb725f3e5f3c6f18043ede0e1c2067218
Lahlukap669 <46345228+Lahlukap669@users.noreply.github.com>
2019-09-23 19:16:45 +0200

    added main code

---
+++ youtube_download_music.py
@@ -0,0 +1,27 @@
+import youtube_dl
+import os
+import shutil
+from ffmpy import FFmpeg
+
+def download_song(song_url):
+      with youtube_dl.YoutubeDL() as ydl:
+            info_dict = ydl.extract_info(song_url, download=True)
+
+      if os.path.exists(os.getcwd() + "/songs")==False:
+            os.mkdir(os.getcwd() + "/songs")
+      
+      for file in os.listdir('.'):
+            if not file.endswith('py'):
+                  try:
+                        filename = file.split(".")[0]
+                        d = os.getcwd()
+                        ff = FFmpeg(executable='C:/ffmpeg/bin/ffmpeg.exe',inputs={file: None}, outputs={"songs/" + filename + ".mp3": None})
+                        ff.run()
+                  except:
+                        pass
+
+      for file in os.listdir('.'):
+            if file.endswith('mp4') or file.endswith('webm') or file.endswith('mkv'):
+                  os.remove(file)
+
+download_song(input("vnes link playlita: "))
