ac3288fcdcd7ebc72f8505f028e36422c811939e
Lahlukap669 <luka1.lah@gmail.com>
2020-03-24 13:32:32 +0100

    working downloaded music

--- youtube_download_music.py
+++ youtube_download_music.py
@@ -6,7 +6,9 @@ from ffmpy import FFmpeg
 def download_song(song_url):
       with youtube_dl.YoutubeDL() as ydl:
             info_dict = ydl.extract_info(song_url, download=True)
-
+      
+      if os.path.exists(os.getcwd() + "/ff")==False:
+            os.mkdir(os.getcwd() + "/ff")
       if os.path.exists(os.getcwd() + "/songs")==False:
             os.mkdir(os.getcwd() + "/songs")
       
@@ -15,13 +17,13 @@ def download_song(song_url):
                   try:
                         filename = file.split(".")[0]
                         d = os.getcwd()
-                        ff = FFmpeg(executable='C:/ffmpeg/bin/ffmpeg.exe',inputs={file: None}, outputs={"songs/" + filename + ".mp3": None})
+                        ff = FFmpeg(executable='ff/ffmpeg/bin/ffmpeg.exe',inputs={file: None}, outputs={"./songs/" + filename + ".mp3": None})
                         ff.run()
                   except:
                         pass
 
-      for file in os.listdir('.'):
-            if file.endswith('mp4') or file.endswith('webm') or file.endswith('mkv'):
-                  os.remove(file)
+##      for file in os.listdir('.'):
+##            if file.endswith('mp4') or file.endswith('webm') or file.endswith('mkv'):
+##                  os.remove(file)
 
 download_song(input("vnes link playlita: "))
